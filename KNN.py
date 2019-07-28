215a7faf446f61b91fc06b0d047081e127ae030f
Hossam Samir <eng.hsamir@gmail.com>
2019-07-28 23:52:26 +0200

    sort distances

--- KNN.py
+++ KNN.py
@@ -1,12 +1,20 @@
 import math
 from collections import Counter
 
+
 def Knn(data, query, k, distance_fn, choice_fn):
     neighbor_distances_and_indices = []
-
     # for each example in data
     for index, example in enumerate(data):
+        # Calculate the distance between the query example and the current
         distance = distance_fn(example[:-1], query)
+        # Add the distance and the index of the example to an ordered collection
+        neighbor_distances_and_indices.append(distance, index)
+
+# Sort the ordered collection of distances and indices from
+# smallest to largest (in ascending order) by the distances
+    sorted_neighbor_distances_and_indices = sorted(
+        neighbor_distances_and_indices)
 
 
 def euclidean_distance(point1, point2):
@@ -21,5 +29,6 @@ def euclidean_distance(point1, point2):
 def mean(labels):
     return sum(labels)/len(labels)
 
+
 def mode(labels):
-        return Counter(labels).most_common(1)[0][0]
+    return Counter(labels).most_common(1)[0][0]
