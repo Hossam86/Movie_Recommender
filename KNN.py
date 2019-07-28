4a5b722e31aa5b40ebef858d40caf47185ac8746
Hossam Samir <eng.hsamir@gmail.com>
2019-07-28 21:12:41 +0200

    distance func

--- KNN.py
+++ KNN.py
@@ -1,4 +1,16 @@
+import math
 def Knn(data, query, k, distance_fn, choice_fn):
     neighbor_distances_and_indices = []
 
-    # for each ea
+    # for each example in data
+    for index, example in enumerate(data):
+        distance = distance_fn(example[:-1], query)
+
+
+def euclidean_distance(point1, point2):
+    # calculate the distance between the query example and the current
+
+    sum_squared_distance = 0
+    for i in range(len(point1)):
+        sum_squared_distance += math.pow(point1[i] - point2[i], 2)
+    return math.sqrt(sum_squared_distance)
